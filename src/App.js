4b312119da3072161a285fb1f05cd11ce96b6141
Tzong-Lian-Tsay <tzong-lian-tsay@lambdastudents.com>
2020-09-11 12:06:37 -0400

    Displayed Character Names

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import axios from 'axios'
 import styled, {keyframes} from 'styled-components'
 
 import './App.css';
-// import Character from './components/Character.js';
+import Character from './components/Character.js';
 
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [charaData, setCharaData] = useState([])
+  const [currentChara, setCurrentChara] = useState('1')
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -29,6 +30,24 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <section className='characters'>
+
+        {charaData.map(ch =>
+          <div className='character' key={ch.id}>
+            {ch.name}
+            {/* <article className='characterInfo'>
+              Name: `${currentChara.name}`
+            </article> */}
+          </div>
+        )}
+
+      </section>
+
+      {/* {
+        charaData.map(ch => {
+          return <Character key={ch.id} info={ch} />
+        })
+      } */}
     </div>
   );
 }
